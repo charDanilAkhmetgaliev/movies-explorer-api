50afcc68fe9b9b7a63a9639fb0b31466d6b7eb2c
Danil' Akhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-05-29 13:16:28 +0300

    fix: add /api endpoint, correcting page not found error

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const { errors } = require('celebrate');
 const router = require('./routes/index');
-const { TOKEN_CONFIG } = require('./config');
+const { TOKEN_CONFIG, OBJECT_ERROR_CONFIG } = require('./config');
 const limiter = require('./middlewares/limiter');
 const corsVerification = require('./middlewares/cors');
+const ObjectNotFoundError = require('./scripts/components/ObjectNotFoundError');
+const { errorHandler } = require('./scripts/utils/error');
+const { errorLogger, requestLogger } = require('./middlewares/logger');
 
 // create server
 const app = express();
@@ -27,8 +31,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(TOKEN_CONFIG.SECRET_JWT));
 
+// connect request logger
+router.use(requestLogger);
+
 // connect main router
-app.use(router);
+app.use('/api', router);
+
+// not found router handler
+app.use((
+  req,
+  res,
+  next,
+) => next(new ObjectNotFoundError(OBJECT_ERROR_CONFIG.PAGE_NOT_FOUND_MESSAGE)));
+
+// connect errors logger
+router.use(errorLogger);
+
+// celebrate errors handler
+router.use(errors());
+
+// connect all errors handler
+app.use((error, req, res, next) => errorHandler(error, res, next));
 
 // start server
 app.listen(PORT);
--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,22 @@
 // create main route
 const router = require('express').Router();
 // import dependencies
-const { errors } = require('celebrate');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
-const { errorHandler } = require('../scripts/utils/error');
-const ObjectNotFoundError = require('../scripts/components/ObjectNotFoundError');
-const { OBJECT_ERROR_CONFIG } = require('../config');
 const { createUser, loginUser, logoutUser } = require('../controllers/users');
 const tokenVerify = require('../middlewares/tokenVerify');
 const { createUserScheme, loginUserScheme } = require('../scripts/utils/clb');
-const { requestLogger, errorLogger } = require('../middlewares/logger');
-
-// connect request logger
-router.use(requestLogger);
 
 // handlers auth routes
 router.post('/signin', loginUserScheme, loginUser);
 router.post('/signup', createUserScheme, createUser);
 
-// connect token verify
-router.use(tokenVerify);
-
 // handler logout route
-router.get('/signout', logoutUser);
+router.get('/signout', tokenVerify, logoutUser);
 
 // connect main routers
-router.use('/users', usersRouter);
-router.use('/movies', moviesRouter);
-
-// not found router handler
-router.use((
-  req,
-  res,
-  next,
-) => next(new ObjectNotFoundError(OBJECT_ERROR_CONFIG.PAGE_NOT_FOUND_MESSAGE)));
-
-// connect errors logger
-router.use(errorLogger);
-
-// celebrate errors handler
-router.use(errors());
-
-// connect all errors handler
-router.use((error, req, res, next) => errorHandler(error, res, next));
+router.use('/users', tokenVerify, usersRouter);
+router.use('/movies', tokenVerify, moviesRouter);
 
 // export main router
 module.exports = router;
