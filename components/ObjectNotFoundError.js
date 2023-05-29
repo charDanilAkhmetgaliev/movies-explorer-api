7d0f51c562215baf73b235fa49782f426af09fa7
Danil' Akhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-05-29 14:16:51 +0300

    resolving merge conflicts

---
+++ components/ObjectNotFoundError.js
@@ -0,0 +1,11 @@
+const { OBJECT_ERROR_CONFIG } = require('../../config');
+
+class ObjectNotFoundError extends Error {
+  constructor(message = OBJECT_ERROR_CONFIG.DEF_MESSAGE) {
+    super(message);
+    this.name = OBJECT_ERROR_CONFIG.ERROR_NAME;
+    this.statusCode = OBJECT_ERROR_CONFIG.STATUS_CODE;
+  }
+}
+
+module.exports = ObjectNotFoundError;
