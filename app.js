4877f2b8ce4b8fc7e00fa7d9efe50c83f12af257
Danil' Akhmetgaliev <akhm3tgal1ev@yandex.ru>
2023-05-30 13:11:36 +0300

    refactor: correct connect request loggers

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const router = require('./routes/index');
 const { TOKEN_CONFIG, MONGO_DB_CONFIG } = require('./config');
 const limiter = require('./middlewares/limiter');
 const corsVerification = require('./middlewares/cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 // create server
 const app = express();
@@ -16,6 +17,9 @@ const { PORT = 3000 } = process.env;
 
 // connect to mongo data base
 mongoose.connect(MONGO_DB_CONFIG.URL);
+
+// connect request logger
+router.use(requestLogger);
 
 // connect protect utilities
 app.use(corsVerification);
@@ -29,6 +33,9 @@ app.use(cookieParser(TOKEN_CONFIG.SECRET_JWT));
 
 // connect main router
 app.use('/api', router);
+
+// connect errors logger
+router.use(errorLogger);
 
 // start server
 app.listen(PORT);
--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,13 @@ const tokenVerify = require('../middlewares/tokenVerify');
 const { createUserScheme, loginUserScheme } = require('../scripts/utils/clb');
 const ObjectNotFoundError = require('../scripts/components/ObjectNotFoundError');
 const { OBJECT_ERROR_CONFIG } = require('../config');
-const { errorLogger, requestLogger } = require('../middlewares/logger');
 const { errorHandler } = require('../scripts/utils/error');
-
-// connect request logger
-router.use(requestLogger);
 
 // handlers auth routes
 router.post('/signin', loginUserScheme, loginUser);
 router.post('/signup', createUserScheme, createUser);
 
+// connect token verification to next routes
 router.use(tokenVerify);
 
 // handler logout route
@@ -34,9 +31,6 @@ router.use((
   res,
   next,
 ) => next(new ObjectNotFoundError(OBJECT_ERROR_CONFIG.PAGE_NOT_FOUND_MESSAGE)));
-
-// connect errors logger
-router.use(errorLogger);
 
 // celebrate errors handler
 router.use(errors());
